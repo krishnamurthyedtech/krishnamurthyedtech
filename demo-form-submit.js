548ef628667477788ed55f98595ede2ccb00bf25
dev4keerthi <dev4keerthi@gmail.com>
2025-06-30 19:13:59 +0530

    changes on time zone

--- demo-form-submit.js
+++ demo-form-submit.js
@@ -1,36 +1,39 @@
-//  var orgId = 1;
-  var orgId = 1;
+$("#submitDemoBtn").click(function () {
+  const orgId = 1;
+  var date = $("#startDate").val();   
+  var time = $("#startTime").val();   
 
-  $("#submitDemoBtn").click(function () {
-    var data = {
-      userName: $("#username").val(),
-      email: $("#email").val(),
-      phoneNumber: $("#mobile").val(),
-      country: $("#country").val(),
-      technology: $("#technology").val(),
-      description: $("#description").val(),
-      startDate: $("#startDate").val()
-    };
+  // Convert to epoch seconds
+  var createdDate = date && time ? Math.floor(new Date(date + 'T' + time).getTime() / 1000) : null;
 
-    $.ajax({
-      // url: "http://localhost:8080/api/v1/" + orgId + "/jobsupport",
-      url:"https://lndhub.krishnamurthyedtechapi.com/api/v1/" + orgId + "/jobsupport/demo",
-      type: "POST",
-      contentType: "application/json",
-      data: JSON.stringify(data),
-      success: function (response) {
-  alert("Demo booked successfully!");
-  setTimeout(function () {
-    $('#demoModal').modal('hide');
-    $('#demoForm')[0].reset();
+  var data = {
+    userName: $("#username").val(),
+    email: $("#email").val(),
+    phoneNumber: $("#mobile").val(),
+    country: $("#country").val(),
+    technology: $("#technology").val(),
+    description: $("#description").val(),
+    createdDate: createdDate ,
+    timeZone: $("#timezone").val()
+  };
 
-    // 🧽 Cleanup leftover backdrop and modal-open class
-    $('body').removeClass('modal-open');
-    $('.modal-backdrop').remove();
-  }, 100);
-},
-      error: function (xhr) {
-        alert("Error: while demo booking" );
-      }
-    });
+  $.ajax({
+    // url: "http://localhost:8080/api/v1/" + orgId + "/jobsupport/demo",
+    url:"https://lndhub.krishnamurthyedtechapi.com/api/v1/" + orgId + "/jobsupport/demo",
+    type: "POST",
+    contentType: "application/json",
+    data: JSON.stringify(data),
+    success: function (response) {
+      alert("Demo booked successfully!");
+      setTimeout(function () {
+        $('#demoModal').modal('hide');
+        $('#demoForm')[0].reset();
+        $('body').removeClass('modal-open');
+        $('.modal-backdrop').remove();
+      }, 100);
+    },
+    error: function (xhr) {
+      alert("Error: while demo booking");
+    }
   });
+});
