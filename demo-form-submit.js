5cb690b0f268daae3e40d1cc8b13fd92a7730dd6
dev4keerthi <dev4keerthi@gmail.com>
2025-07-10 19:17:33 +0530

    spinner changes

--- demo-form-submit.js
+++ demo-form-submit.js
@@ -1,11 +1,12 @@
 $("#submitDemoBtn").click(function () {
   console.log("Submit button clicked");
   const orgId = 1;
-  //  const orgId = 402;
   var date = $("#startDate").val();   
   var time = $("#startTime").val();   
+  var $btn = $(this);
+  var $spinner = $("#submitSpinner");
+  var $submitText = $("#submitText");
 
-  // Convert to epoch seconds
   var createdDate = date && time ? Math.floor(new Date(date + 'T' + time).getTime() / 1000) : null;
 
   var data = {
@@ -15,16 +16,25 @@ $("#submitDemoBtn").click(function () {
     country: $("#country").val(),
     technology: $("#technology").val(),
     description: $("#description").val(),
-    createdDate: createdDate ,
-    timeZone: $("#timezone").val()
+    createdDate: createdDate,
+    timeZone: $("#timezone").val(),
+    // link: $("#link").val("https://pioneermeetings.com/?roomId="),
   };
+
+  // Immediately show loading state
+  $btn.prop('disabled', true);
+  $submitText.text("Processing..."); // Change button text
+  $spinner.removeClass('d-none');
 
   $.ajax({
     // url: "http://localhost:8080/api/v1/" + orgId + "/jobsupport/demo",
-    url:"https://lndhub.krishnamurthyedtechapi.com/api/v1/" + orgId + "/jobsupport/demo",
+    url: "https://lndhub.krishnamurthyedtechapi.com/api/v1/" + orgId + "/jobsupport/demo",
     type: "POST",
     contentType: "application/json",
     data: JSON.stringify(data),
+    beforeSend: function () {
+      $("#loader").show(); // Page-level loader
+    },
     success: function (response) {
       alert("Demo booked successfully!");
       setTimeout(function () {
@@ -37,6 +47,13 @@ $("#submitDemoBtn").click(function () {
     },
     error: function (xhr) {
       alert("Error: while demo booking");
+    },
+    complete: function () {
+      // Reset button state
+      $btn.prop('disabled', false);
+      $submitText.text("Submit");
+      $spinner.addClass('d-none');
+      $("#loader").hide();
     }
   });
 });
