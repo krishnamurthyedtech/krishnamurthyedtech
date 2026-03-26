25fb6e69f69acdb6239c8b58364ee3a156e00e36
dev4vasanthkumar <dev4vasanthkumar@gmail.com>
2026-03-26 11:23:09 +0530

    KMEdtech-UI_code

--- api.ts
+++ api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
